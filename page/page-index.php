7e7e1658178f32be3b7bae05ffdf527058ab4a25
kevin50406418 <kevin50406418@yahoo.com.tw>
2015-05-26 01:22:15 +0800

    1.0.6.1
    fix bug

--- page/page-index.php
+++ page/page-index.php
@@ -1,2 +1,24 @@
 <?php if(!defined('IN_CCS')) {exit('Access Denied');}?>
-<h2>Main</h2>
+<?php setlocale(LC_MONETARY, 'zh_TW');?>
+<div class="ui cards">
+	<?php foreach ($items as $key => $item) {?>
+    <div class="card">
+        <a class="image">
+            <?php if(file_exists(ABSPATH."img/".$item["item_img"]) && !empty($item["item_img"])){?>
+				<img class="ui tiny middle aligned bordered image" src="img/<?php echo $item["item_img"]?>">
+			<?php }else{?>
+				<i class="bordered big photo icon"></i>
+			<?php }?>
+        </a>
+        <div class="content">
+            <a class="header"><?php echo $item['item_name']?></a>
+            <div class="meta">
+                <a><?php echo $item['store_name']?></a>
+            </div>
+            <div class="description blue">
+                <span class="right floated"><?php echo money_format('%(#10.0n', $item['item_price']) ?></span>
+            </div>
+        </div>
+    </div>
+    <?php }?>
+</div>
--- page/page-sysop.php
+++ page/page-sysop.php
@@ -2,7 +2,7 @@
 	$admin_store = array('item','store','type','edititem','edittype','editshop');
 ?>
 <div class="ui inverted segment">
-	<a href="sysop.php?act=store" class="ui button inverted orange<?php if( in_array($_GET['act'],$admin_store)){?> active<?php }?>">商店管理</a>
-	<a href="sysop.php?act=order" class="ui button inverted teal<?php if($_GET['act']=="order"){?> active<?php }?>">訂單管理</a>
-	<a href="sysop.php?act=member" class="ui button inverted purple<?php if($_GET['act']=="member"){?> active<?php }?>">會員管理</a>
+	<a href="sysop.php?act=store" class="ui button inverted orange<?php if(isset($_GET['act']))if( in_array($_GET['act'],$admin_store)){?> active<?php }?>">商店管理</a>
+	<a href="sysop.php?act=order" class="ui button inverted teal<?php if(isset($_GET['act']))if($_GET['act']=="order"){?> active<?php }?>">訂單管理</a>
+	<a href="sysop.php?act=member" class="ui button inverted purple<?php if(isset($_GET['act']))if($_GET['act']=="member"){?> active<?php }?>">會員管理</a>
 </div>
